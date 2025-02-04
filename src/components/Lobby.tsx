0bb0d987eff9a5e9a7110f277d401415464a6164
NeiderG <cosistemasysoftware@uninunez.edu.co>
2025-02-04 18:06:03 -0500

    Update references from Lottery to RaffleNG in AboutGame and HowToUse components; add introductory section in Lobby component

--- src/components/Lobby.tsx
+++ src/components/Lobby.tsx
@@ -91,6 +91,66 @@ const Lobby = () => {
                         </div>
                     </div>
             }
+
+
+            <div className="border-t-4 rounded-4xl border-gray-100/20 my-20"></div>
+
+            <div className="container md:max-w-4xl mx-auto mb-8 ">
+
+                <div className="text-center">
+                    <div className="bg-white/50 backdrop-blur-sm rounded-4xl p-6 mb-6 mt-10">
+                        <h2 className="text-4xl font-poetsen font-extrabold text-gray-800 mb-3">
+                            Get Started
+                        </h2>
+                        <h2 className="text-gray-700 text-center text-md font-semibold my-10">
+                            RaffleNG is a free web application designed to randomly select winners from a list of participants. Perfect for contests, giveaways, and fair selection processes.
+                        </h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-gray-200 mt-10">
+                            <div className="p-4 text-gray-700 bg-gray-100/20 rounded-2xl">
+                                <h3 className="text-2xl font-poetsen font-bold text-indigo-500 mb-4">Excel Upload</h3>
+                                <p>Upload your participant list using an Excel file for quick setup</p>
+
+                                <p className="underline mt-2">File can have one or two columns</p>
+
+                                <div className="overflow-hidden rounded-lg mt-4">
+                                    <table className="text-left rounded-2xl w-full text-gray-400">
+                                        <thead>
+                                            <tr className="bg-gray-200">
+                                                <th className="p-2 border-r border-white">Name</th>
+                                                <th className="p-2 ">Last Name (optional)</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr className="bg-gray-100">
+                                                <td className="px-2 border-r border-white">Participant name</td>
+                                                <td className="px-2 ">Participant last name</td>
+                                            </tr>
+                                            <tr className="bg-gray-100">
+                                                <td className="px-2 border-r border-white">Others...</td>
+                                                <td className="px-2 ">Others...</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div className="p-4 text-gray-700 bg-gray-100/20 rounded-2xl">
+                                <h3 className="text-2xl mb-4 text-indigo-500 font-poetsen font-bold">Manual Entry</h3>
+                                <p>Enter participants manually one by one or paste a list</p>
+                                <p className="underline mt-2">At least 3 participants</p>
+
+                                <div className="overflow-hidden rounded-lg mt-4">
+                                    <div className="bg-gray-100 text-gray-400 text-left p-3">
+                                        <p>Participant One</p>
+                                        <p>Participant Two</p>
+                                        <p>Participant Tree</p>
+                                        <p>Participant ...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
